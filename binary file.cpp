d93c687b81b04832c7ed8b964c06e418bb6a52b3
Einarsk <einarsk@gmail.com>
2016-12-03 17:10:07 +0200

    Lēnām iet uz priekšu
    Mēģinu saprast kas un kā notiek. Lēnām mainu lietas.

--- binary file.cpp
+++ binary file.cpp
@@ -5,36 +5,46 @@ using namespace std;
 //#include <aeslib.h>
 #include <aeslib.c>
 
-//test
+int const matrix_size = 15; //total of 16 elements
+
+
+
+
 
 int main(int argc, char *argv[])
 {
     /*std::string *state    = new string("Bulk copy");
     std::string *roundkey = new string();*/
-    char psc [] = "This is Cyrpto";
-    char * state = psc;
-    char psc2[] = {1,1,1,1,1,1,1,1,0};
-    char * roundkey = psc2;
-    AddRoundKey(psc, 9, psc2);
-    cout << psc;
-    return 0;
-    /*int x;
-    int y;
-    cout << "This is bit operation tutorial. Input numbers or chars: \n";
-    cout << "variable x: ";
-    cin >> x;
-    cout << "variable y: ";
-    cin >> y;
-    int OR = x | y ;
-    int AND = x & y;
-    int XOR = x ^ y;
-    int YBitsLeft = x << y;
-    int YBitsRight = x >> y;
-    cout << "x OR y: " << OR << endl;
-    cout << "x XOR y: " << XOR << endl;
-    cout << "x AND y: " << AND << endl;
-    cout << "x with push Y bits left: " << YBitsLeft << endl;
-    cout << "x with pull Y bits Right: " << YBitsRight << endl;
-    cout << "flip x: " << ~x << endl;
-    return 0;*/
-}
+
+	///Vajag paddot ctypto tekstu ja garums nav pietiekams, tad jaizpilda ar 0!. Ka ari ko darit, ja ir parak gars?
+    char crypto_text [] = "This is Cyrpto";
+	char * state = crypto_text;
+    char key[] = "111111111111110";
+    char * roundkey = key;
+	
+
+   AddRoundKey(crypto_text, key);
+	cout << crypto_text << endl;;
+    
+    //int x;
+    //int y;
+    //cout << "This is bit operation tutorial. Input numbers or chars: \n";
+    //cout << "variable x: ";
+    //cin >> x;
+    //cout << "variable y: ";
+    //cin >> y;
+    //int OR = x | y ;
+    //int AND = x & y;
+    //int XOR = x ^ y;
+    //int YBitsLeft = x << y;
+    //int YBitsRight = x >> y;
+    //cout << "x OR y: " << OR << endl;
+    //cout << "x XOR y: " << XOR << endl;
+    //cout << "x AND y: " << AND << endl;
+    //cout << "x with push Y bits left: " << YBitsLeft << endl;
+    //cout << "x with pull Y bits Right: " << YBitsRight << endl;
+    //cout << "flip x: " << ~x << endl;
+    
+	system("pause");
+	return 0;
+};
